256dc289911c07eacc30f24f0461da7f4ea90044
Khan-Amit <amit.khanna.1082@gmail.com>
2026-05-28 19:13:57 +0700

    Added collector and telemetry source

---
+++ collector.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    printf("Collector: Starting data collection...\n");
+    // Add your collection logic here
+    return 0;
+}
---
+++ telemetry.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    printf("Telemetry: Reading sensor data...\n");
+    // Add your telemetry logic here
+    return 0;
+}
